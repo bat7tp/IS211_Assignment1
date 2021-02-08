2c94d8a9daf1e3929ca90f8f2c7ed73606528fd4
Batsheva Paul <batsheva.paul50@spsmail.cuny.edu>
2021-02-08 11:15:53 -0500

    first commit

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,44 @@
 
-def listDivide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
+# Part 1 of Assignment 1
+
+
+def list_divide(numbers, divide=2):
+    list = []
+    for x in numbers:
+        if x % divide == 0:
+            list.append(x)
+    return (len(list))
+
+
+def test_list_divide():
+    a = list_divide([1, 2, 3, 4, 5])
+
+    if a != 2:
+        raise List_Divide_Exception()
+
+    b = list_divide([2, 4, 6, 8, 10])
+
+    if b != 5:
+        raise List_Divide_Exception()
+
+    c = list_divide([30, 54, 63, 98, 100], divide=10)
+
+    if c != 2:
+        raise List_Divide_Exception()
+
+    d = list_divide([])
+
+    if d != 0:
+        raise List_Divide_Exception()
+
+    e = list_divide([1, 2, 3, 4, 5], 1)
+
+    if e != 5:
+        raise List_Divide_Exception()
+
+
+class List_Divide_Exception(Exception):
     pass
 
-def testListDivide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
-if __name__ == "__main__":
-    testListDivide()
+
+test_list_divide()
