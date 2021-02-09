af2824ba2d30fed7e15b9d693be3b3c55fa2982f
Batsheva Paul <batsheva.paul50@spsmail.cuny.edu>
2021-02-08 21:48:24 -0500

    This is the completed part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,21 @@
 
+#create book class with the two attributes
 class Book:
-    def __init(self, author, title):
-        pass
-    
-    def display(self):
-        pass
-        
-if __name__ == "__main__":
-    pass
+  def __init__(self, author, title):
+
+    self.author = author
+    self.title = title
+
+#create function to display written by book and title format
+  def display(self):
+
+    print (self.title + ", written by " + self.author)
+
+#creating objects
+book1 = Book("John Steinbeck", "Of Mice and Men")
+book2 = Book("Robert Lee", "To Kill A Mockingbird")
+
+
+book1.display()
+book2.display()
+
